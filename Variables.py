c4f0ceaae4feda14e97fb4c351116e0e4695347b
Андрей <omarov.a9609@gmail.com>
2024-11-08 14:02:16 +0500

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -3,5 +3,5 @@ NumberOfHours = 1.5
 CourseName = 'Python'
 AverageTime = NumberOfHours / Homework #среднее время выполнения
 print(
-    f'Курс: {CourseName}, всего задач {Homework}, затрачено часов {NumberOfHours}, среднее время выполнения 
-        {AverageTime} часов. ')
+    f'Курс: {CourseName}, всего задач {Homework}, затрачено часов {NumberOfHours}, среднее время выполнения '
+    f'{AverageTime} часов. ')
