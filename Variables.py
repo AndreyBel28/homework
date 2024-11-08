7456fa964277b0e847f581ab8cd3e07b8bf231f9
AndreyBel28 <omarov.a9609@gmail.com>
2024-11-08 09:18:12 +0500

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,6 @@
+Homework = 12
+NumberOfHours = 1.5
+CourseName = 'Python'
+AverageTime = NumberOfHours / Homework #среднее время выполнения
+print(
+    f'Курс: {CourseName}, всего задач {Homework}, затрачено часов {NumberOfHours}, среднее время выполнения {AverageTime} часов. ')
